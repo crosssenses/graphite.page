aea88e439d9abec4b88ff6bc4e71d9b9af514cd6
crosssenses <lari_wu@gmx.de>
2020-11-27 19:19:15 +0100

    build ethics

---
+++ _scripts/ethics.sh
@@ -0,0 +1,8 @@
+
+# Copy papers
+cp -a ../ethics-of-digitalisation/policy-brief-audits/_build/. policy-brief-audits
+cp -a ../ethics-of-digitalisation/policy-brief-blackbox/_build/. policy-brief-blackbox
+cp -a ../ethics-of-digitalisation/policy-brief-values/_build/. policy-brief-values
+
+# Copy root static till static fixed
+cp -a ../ethics-of-digitalisation/static/. static
